1266da53a12d0adb292fabb6f54a72977f7bfa14
ud2 <sjx233@qq.com>
2023-11-16 19:45:50 +0800

    delete dom_exception.ts

--- dom_exception.ts
+++
@@ -1,9 +0,0 @@
-delete (DOMException.prototype as unknown as Record<symbol, unknown>)[
-  Symbol.for("Deno.customInspect")
-];
-
-export function domException(message?: string, name?: string): DOMException {
-  const error = new DOMException(message, name);
-  Error.captureStackTrace?.(error, domException);
-  return error;
-}
--- interrupt_signal.ts
+++ interrupt_signal.ts
@@ -1,11 +1,9 @@
-import { domException } from "./dom_exception.ts";
-
 const controller = new AbortController();
 Deno.addSignalListener("SIGINT", function abort() {
   queueMicrotask(() => {
     Deno.addSignalListener("SIGINT", () => Deno.exit(0x82));
     Deno.removeSignalListener("SIGINT", abort);
   });
-  controller.abort(domException("Interrupted", "AbortError"));
+  controller.abort(new DOMException("Interrupted", "AbortError"));
 });
 export const signal = controller.signal;
